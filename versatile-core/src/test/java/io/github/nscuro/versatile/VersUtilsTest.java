e30ca0fc998272105ff0024ca61aa3fd1807bf21
Niklas <nscuro@protonmail.com>
2024-06-19 22:05:58 +0200

    Archive integration test results (#92)
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- versatile-core/src/test/java/io/github/nscuro/versatile/VersUtilsTest.java
+++ versatile-core/src/test/java/io/github/nscuro/versatile/VersUtilsTest.java
@@ -18,45 +18,17 @@
  */
 package io.github.nscuro.versatile;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.jayway.jsonpath.Configuration;
-import com.jayway.jsonpath.DocumentContext;
-import com.jayway.jsonpath.JsonPath;
-import com.jayway.jsonpath.spi.json.JacksonJsonNodeJsonProvider;
-import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
-import io.github.jeremylong.openvulnerability.client.ghsa.GitHubSecurityAdvisoryClient;
-import io.github.jeremylong.openvulnerability.client.ghsa.SecurityAdvisory;
-import io.github.jeremylong.openvulnerability.client.ghsa.Vulnerabilities;
-import io.github.nscuro.versatile.version.InvalidVersionException;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 
-import static io.github.jeremylong.openvulnerability.client.ghsa.GitHubSecurityAdvisoryClientBuilder.aGitHubSecurityAdvisoryClient;
 import static io.github.nscuro.versatile.VersUtils.schemeFromGhsaEcosystem;
 import static io.github.nscuro.versatile.VersUtils.schemeFromOsvEcosystem;
 import static io.github.nscuro.versatile.VersUtils.versFromGhsaRange;
@@ -65,7 +37,6 @@ import static io.github.nscuro.versatile.VersUtils.versFromOsvRange;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.assertj.core.api.Assertions.assertThatNoException;
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class VersUtilsTest {
@@ -194,117 +165,6 @@ class VersUtilsTest {
             assertThat(schemeFromOsvEcosystem(ecosystem)).isEmpty();
         } else {
             assertThat(schemeFromOsvEcosystem(ecosystem)).contains(expectedScheme);
-        }
-    }
-
-    @Test
-    @Disabled
-    void testVersFromGhsaRangeWithAllRanges() throws Exception {
-        assumeTrue(System.getenv("GITHUB_TOKEN") != null, "GITHUB_TOKEN must be set");
-
-        final GitHubSecurityAdvisoryClient ghsaClient = aGitHubSecurityAdvisoryClient()
-                .withApiKey(System.getenv("GITHUB_TOKEN"))
-                .build();
-
-        final var arrayNode = JsonNodeFactory.instance.arrayNode();
-
-        try (ghsaClient) {
-            while (ghsaClient.hasNext()) {
-                Collection<SecurityAdvisory> result = ghsaClient.next();
-                if (result == null || result.isEmpty()) {
-                    continue;
-                }
-
-                result.stream()
-                        .map(SecurityAdvisory::getVulnerabilities)
-                        .map(Vulnerabilities::getEdges)
-                        .flatMap(Collection::stream)
-                        .map(edge -> JsonNodeFactory.instance.objectNode()
-                                .put("ecosystem", edge.getPackage().getEcosystem())
-                                .put("package", edge.getPackage().getName())
-                                .put("range", edge.getVulnerableVersionRange())
-                                .put("vers", versFromGhsaRange(edge.getPackage().getEcosystem(), edge.getVulnerableVersionRange()).toString()))
-                        .forEach(arrayNode::add);
-            }
-        }
-
-        try (final var fos = Files.newOutputStream(Paths.get("ghsa.json"))) {
-            new ObjectMapper().writerWithDefaultPrettyPrinter().writeValue(fos, arrayNode);
-        }
-    }
-
-    @Disabled
-    @ParameterizedTest
-    @ValueSource(strings = {
-            "Debian",
-            "Go",
-            "Maven",
-            "npm"
-    })
-    void testVersFromOsvRangeWithAllRanges(final String ecosystem) throws Exception {
-        final Path tempFile = Files.createTempFile(null, null);
-
-        final HttpRequest request = HttpRequest.newBuilder().GET()
-                .uri(URI.create("https://osv-vulnerabilities.storage.googleapis.com/%s/all.zip".formatted(ecosystem)))
-                .build();
-        final HttpResponse<Path> response = HttpClient.newHttpClient()
-                .send(request, HttpResponse.BodyHandlers.ofFile(tempFile));
-        assertThat(response.statusCode()).isEqualTo(200);
-
-        final var objectMapper = new ObjectMapper();
-        final var jsonPathConfig = Configuration.defaultConfiguration()
-                .jsonProvider(new JacksonJsonNodeJsonProvider(objectMapper))
-                .mappingProvider(new JacksonMappingProvider(objectMapper));
-
-        final var arrayNode = objectMapper.createArrayNode();
-
-        try (final var zipFile = new ZipFile(response.body().toFile())) {
-            final Enumeration<? extends ZipEntry> entries = zipFile.entries();
-            while (entries.hasMoreElements()) {
-                final DocumentContext jsonPathCtx;
-                try (final var entryInputStream = zipFile.getInputStream(entries.nextElement())) {
-                    jsonPathCtx = JsonPath.parse(entryInputStream, jsonPathConfig);
-                }
-
-                final ArrayNode affectedWithPurlAndRanges =
-                        jsonPathCtx.read("$.affected[?(!@.package.purl.empty && !@.ranges.empty)]");
-                if (affectedWithPurlAndRanges.isEmpty()) {
-                    continue;
-                }
-
-                for (final JsonNode affected : affectedWithPurlAndRanges) {
-                    final ArrayNode ranges = (ArrayNode) affected.get("ranges");
-                    if (ranges == null) {
-                        continue;
-                    }
-
-                    for (final JsonNode range : ranges) {
-                        if ("GIT".equals(range.get("type").asText())) {
-                            continue;
-                        }
-
-                        final var events = new ArrayList<Map.Entry<String, String>>();
-                        for (final JsonNode eventNode : range.get("events")) {
-                            final String fieldName = eventNode.fieldNames().next();
-                            events.add(Map.entry(fieldName, eventNode.get(fieldName).asText()));
-                        }
-
-                        try {
-                            final Vers vers = versFromOsvRange(range.get("type").asText(), ecosystem, events, null);
-                            arrayNode.add(objectMapper.createObjectNode()
-                                    .put("name", affected.get("package").get("name").asText())
-                                    .putPOJO("events", events)
-                                    .put("vers", vers.toString()));
-                        } catch (VersException | InvalidVersionException e) {
-                            System.out.println("Failed to convert range %s: %s".formatted(range, e));
-                        }
-                    }
-                }
-            }
-        }
-
-        try (final var fos = Files.newOutputStream(Path.of("osv-%s.json".formatted(ecosystem)))) {
-            objectMapper.writerWithDefaultPrettyPrinter().writeValue(fos, arrayNode);
         }
     }
 
